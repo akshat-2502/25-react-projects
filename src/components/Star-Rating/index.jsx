d0273b6019503693d294b0169bdc89b8706168ca
akshat-2502 <khulbeakshat@gmail.com>
2025-05-18 02:14:38 +0530

    star project almost done

--- src/components/Star-Rating/index.jsx
+++ src/components/Star-Rating/index.jsx
@@ -5,6 +5,18 @@ export default function StarRating({ noOfStar = 5 }) {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
 
+  const handleClick = (getCurrentIndex) => {
+    setRating(getCurrentIndex);
+  };
+
+  const handleMouseEnter = (getCurrentIndex) => {
+    setHover(getCurrentIndex);
+  };
+
+  const handleMouseLeave = () => {
+    setHover(rating);
+  };
+
   return (
     <div className="star-rating">
       {[...Array(noOfStar)].map((_, index) => {
