05cf99e2d38235b0b9270877a857446c54ca3450
akshat-2502 <khulbeakshat@gmail.com>
2025-05-18 02:11:35 +0530

    star project working

--- src/components/Star-Rating/index.jsx
+++ src/components/Star-Rating/index.jsx
@@ -1,3 +1,25 @@
-export default function StarRating() {
-  return <div></div>;
+import { useState } from "react";
+import { FaStar } from "react-icons/fa";
+
+export default function StarRating({ noOfStar = 5 }) {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  return (
+    <div className="star-rating">
+      {[...Array(noOfStar)].map((_, index) => {
+        index += 1;
+
+        return (
+          <FaStar
+            key={index}
+            onClick={() => handleClick(index)}
+            onMouseMove={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave()}
+            size={40}
+          />
+        );
+      })}
+    </div>
+  );
 }
