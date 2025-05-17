e213ee18a606d07e72354be4d73b88a165e912eb
akshat-2502 <khulbeakshat@gmail.com>
2025-05-18 02:17:25 +0530

    only styling left

--- src/components/Star-Rating/index.jsx
+++ src/components/Star-Rating/index.jsx
@@ -25,6 +25,7 @@ export default function StarRating({ noOfStar = 5 }) {
         return (
           <FaStar
             key={index}
+            className={index <= (hover || rating) ? "active" : "inactive"}
             onClick={() => handleClick(index)}
             onMouseMove={() => handleMouseEnter(index)}
             onMouseLeave={() => handleMouseLeave()}
