f8e07ff80ef93fbdfa03cbbd6a19a3c81fcb3a4e
akshat-2502 <khulbeakshat@gmail.com>
2025-05-20 23:58:12 +0530

    fething images

--- src/components/Image-Slider/index.jsx
+++ src/components/Image-Slider/index.jsx
@@ -1,8 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function ImageSlider({ url, limit }) {
   const [images, setImages] = useState([]);
   const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    if (url !== "") fetchImages(url);
+  }, [url]);
 
   return <div className="container"></div>;
 }
