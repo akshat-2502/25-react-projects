bab222863e4ce9533dce8f167d42a30efbb217a5
akshat-2502 <khulbeakshat@gmail.com>
2025-05-22 00:58:46 +0530

    images have been fetched

--- src/components/Load-More-Button/index.jsx
+++ src/components/Load-More-Button/index.jsx
@@ -33,5 +33,21 @@ export default function LoadMoreButton() {
     return <div>Data is Loading ! Please Wait </div>;
   }
 
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <div className="product-container">
+        {products && products.length
+          ? products.map((item) => (
+              <div className="product" key={item.id}>
+                <img src={item.thumbnail} alt={item.title} />
+                <p>{item.title}</p>
+              </div>
+            ))
+          : null}
+      </div>
+      <div className="button-container">
+        <button>Load More</button>
+      </div>
+    </div>
+  );
 }
