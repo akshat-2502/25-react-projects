df1b75f0114a02662b01230e1a9d0db6e0206203
akshat-2502 <khulbeakshat@gmail.com>
2025-05-27 00:22:53 +0530

    comitcomit

--- src/components/Github-Profile-Finder/user.jsx
+++ src/components/Github-Profile-Finder/user.jsx
@@ -1,5 +1,14 @@
 const User = ({ user }) => {
-  const { avatar_url, followers, following, public_repos, name, login } = user;
+  const {
+    avatar_url,
+    followers,
+    following,
+    public_repos,
+    name,
+    login,
+    created_at,
+  } = user;
+  const createdDate = new Date(created_at);
   return (
     <div className="user">
       <div>
@@ -9,6 +18,12 @@ const User = ({ user }) => {
         <a target="_blank" href={`https://www.github.com/${login}`}>
           {name || login}
         </a>
+        <p>
+          User Joined ON :-{" "}
+          {`${createdDate.getDate()} ${createdDate.toLocaleString("en-us", {
+            month: "short",
+          })} ${createdDate.getFullYear()}`}
+        </p>
       </div>
     </div>
   );
