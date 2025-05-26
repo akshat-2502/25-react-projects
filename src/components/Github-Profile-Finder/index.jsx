446c5c65f1b14fc6b3b5eece3bf68593a7113d14
akshat-2502 <khulbeakshat@gmail.com>
2025-05-26 23:32:55 +0530

    github profile finder started

---
+++ src/components/Github-Profile-Finder/index.jsx
@@ -0,0 +1,12 @@
+const GithubProfileFinder = () => {
+  return (
+    <div>
+      <h1>Github Profile Finder</h1>
+      <div className="github-profile-container">
+        <div className="input-wrapper"></div>
+      </div>
+    </div>
+  );
+};
+
+export default GithubProfileFinder;
