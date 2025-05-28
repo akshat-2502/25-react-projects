55bdaf92f89621cc92903b11809cea5c5d841fe8
akshat-2502 <khulbeakshat@gmail.com>
2025-05-29 00:42:02 +0530

    completed

--- src/components/Scroll-to top-bottom/index.jsx
+++ src/components/Scroll-to top-bottom/index.jsx
@@ -1,21 +1,48 @@
+import { useRef } from "react";
 import useFetch from "../Use-Fetch-Custom-Hooks";
 
 const ScrollToTopAndBottom = () => {
   const { data, error, loading } = useFetch(
-    "https://dummyjson.com/products",
+    "https://dummyjson.com/products?limit=100",
     {}
   );
 
+  const bottomRef = useRef(null);
+
+  const handleScrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
+
+  const handleScrollToBottom = () => {
+    bottomRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
-    <div>
-      <h1>Scroll To Bottom and Top</h1>
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-4xl font-bold mb-8">Scroll To Bottom and Top</h1>
       <h3>This is the top section</h3>
-      <button>Scroll To Bottom</button>
-      <ul>
+      <button
+        onClick={handleScrollToBottom}
+        className="bg-blue-500 text-white cursor-pointer px-6 rounded-lg m-4 py-2"
+      >
+        Scroll To Bottom
+      </button>
+      <ul className="text-center">
         {data && data.products && data.products.length
           ? data.products.map((item) => <li>{item.title}</li>)
           : null}
       </ul>
+      <button
+        onClick={handleScrollToTop}
+        className="bg-blue-500 cursor-pointer text-white px-6 rounded-lg m-4 py-2"
+      >
+        Scroll To Top
+      </button>
+      <div ref={bottomRef}>This is Bottom</div>
     </div>
   );
 };
