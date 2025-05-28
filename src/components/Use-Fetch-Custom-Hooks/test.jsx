d15e6492a2244c4761e9059e938de82391080523
akshat-2502 <khulbeakshat@gmail.com>
2025-05-28 22:20:31 +0530

    errors removed

--- src/components/Use-Fetch-Custom-Hooks/test.jsx
+++ src/components/Use-Fetch-Custom-Hooks/test.jsx
@@ -9,12 +9,10 @@ const UseFetchHookTest = () => {
   return (
     <div>
       <h1>Use Fetch Hook</h1>
-      {loading ? <h3>Loading ! Please Wait</h3> : null};
-      {error ? <h3>{error}</h3> : null};
+      {loading ? <h3>Loading ! Please Wait</h3> : null}
+      {error ? <h3>{error}</h3> : null}
       {data && data.products && data.products.length
-        ? data.products.map((item) => {
-            <p key={item.key}>{item.title}</p>;
-          })
+        ? data.products.map((item) => <p>{item.title}</p>)
         : null}
     </div>
   );
